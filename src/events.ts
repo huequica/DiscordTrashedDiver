56236f969b1b067b181e70ccccd13656d10cb089
huequica <dev@huequica.xyz>
2022-03-01 20:28:05 +0900

    split event subscribe space

---
+++ src/events.ts
@@ -0,0 +1,14 @@
+import { Client } from 'discord.js';
+
+/**
+ * イベントを登録していくための空間
+ * @param client イベント登録前の Client オブジェクト
+ * @return client イベントを登録したものを返す
+ */
+export const subscribeEvents = (client: Client): Client => {
+  client.once('ready', () => {
+    console.log('start discord bot service!');
+  });
+
+  return client;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { Client, Intents } from 'discord.js';
 import { DISCORD_TOKEN } from './config/env';
+import { subscribeEvents } from './events';
 
 const main = async () => {
-  const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-  client.once('ready', () => {
-    console.log('start discord bot service!');
-  });
+  const client = subscribeEvents(
+    new Client({ intents: [Intents.FLAGS.GUILDS] })
+  );
 
   await client.login(DISCORD_TOKEN);
 };
