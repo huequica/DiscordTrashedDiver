048f396491e39e6bd9cd839aff88404aacb5249f
huequica <dev@huequica.xyz>
2022-03-05 05:47:12 +0900

    create error mock and mockGenerator function

---
+++ src/lib/mocks/errors.ts
@@ -0,0 +1,20 @@
+import {
+  ApiRequestError,
+  ApiResponseError,
+  TwitterApiErrorData,
+} from 'twitter-api-v2';
+import { ClientRequest, IncomingHttpHeaders, IncomingMessage } from 'http';
+
+export const mockTwitterApiRequestError = new ApiRequestError('mock error!', {
+  request: {} as unknown as ClientRequest,
+  error: new Error(),
+});
+
+export const generateMockTwitterApiResponseError = (code: number) =>
+  new ApiResponseError('mock error!', {
+    request: {} as unknown as ClientRequest,
+    response: {} as unknown as IncomingMessage,
+    headers: {} as unknown as IncomingHttpHeaders,
+    data: {} as unknown as TwitterApiErrorData,
+    code,
+  });
