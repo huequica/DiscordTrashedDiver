b70eaacc14f674003baf0fa5736db386fcaae87f
huequica <dev@huequica.xyz>
2022-03-05 04:28:36 +0900

    create twitter tokens mock

---
+++ src/lib/mocks/env.ts
@@ -0,0 +1,12 @@
+import { TWITTER_TOKENS } from '@/config/env';
+
+export const mockTwitterTokens: ReturnType<typeof TWITTER_TOKENS> = {
+  consumer: {
+    key: 'ssss',
+    secret: 'ssss',
+  },
+  account: {
+    key: 'ssss',
+    secret: 'ssss',
+  },
+};
