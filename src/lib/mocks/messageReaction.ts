3cc0bf36f9ba74af0327b4483c3862cc336bdac9
huequica <dev@huequica.xyz>
2022-03-06 02:27:28 +0900

    create emojis mock

--- src/lib/mocks/messageReaction.ts
+++ src/lib/mocks/messageReaction.ts
@@ -1,12 +1,35 @@
-import { MessageReaction } from 'discord.js';
+import { GuildEmoji, MessageReaction } from 'discord.js';
+
+const mockGuildEmojis = (omitExpectEmoji: boolean): GuildEmoji[] => {
+  const objects = new Array(3).map(
+    (_, index) =>
+      ({
+        name: `mockEmoji_${index}}`,
+        toString: () => `:mockEmoji_${index}:`,
+      } as GuildEmoji)
+  );
+
+  // true なら正常例の絵文字オブジェクトも追加
+  if (!omitExpectEmoji)
+    objects.push({
+      name: 'boomerang',
+      toString: () => ':boomerang:',
+    } as GuildEmoji);
+
+  return objects;
+};
 
 interface MockMessageReactionOptions {
+  message: {
+    content?: string;
+  };
   channel: {
     name?: string;
     type?: string;
   };
   emoji: {
     name?: string;
+    omitExpectEmoji?: boolean;
   };
 }
 
@@ -18,6 +41,7 @@ export const generateMockMessageReaction = (
 ): MessageReaction => {
   return {
     message: {
+      content: options?.message?.content || 'mockMessage!',
       channel: {
         type: options?.channel?.type || 'GUILD_TEXT',
         name: options?.channel?.name || 'ごみばこ',
@@ -29,6 +53,11 @@ export const generateMockMessageReaction = (
       // モックで適当に toString をオーバーライドして返却値を設定
       toString() {
         return `:${this.name}:`;
+      },
+    },
+    client: {
+      emojis: {
+        cache: mockGuildEmojis(options?.emoji?.omitExpectEmoji || false),
       },
     },
   } as unknown as MessageReaction;
