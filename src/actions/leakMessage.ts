84c95cef960b0029a5419b839d415ea635aadaa2
huequica <dev@huequica.xyz>
2022-03-06 01:33:49 +0900

    implement tweet message and error handling

--- src/actions/leakMessage.ts
+++ src/actions/leakMessage.ts
@@ -1,13 +1,19 @@
 import { MessageReaction } from 'discord.js';
 import { shouldRunLeak } from '@/actions/utils/leakMessage/shouldRunLeak';
+import { pickEmoji } from '@/actions/utils/pickEmoji';
+import { TwitterService } from '@/lib/services/twitter';
+import {
+  EmojiNotFoundError,
+  NetworkHandshakeException,
+  ServerErrorException,
+  UnauthorizedException,
+} from '@/lib/exceptions';
 import {
   isTextChannel,
   getChannelFromReaction,
 } from '@/typeGuards/isTextChannel';
-import { EmojiNotFoundError } from '@/lib/exceptions';
-import { pickEmoji } from '@/actions/utils/pickEmoji';
 
-export const leakMessage = (reaction: MessageReaction) => {
+export const leakMessage = async (reaction: MessageReaction) => {
   // 原則として来ることはないがコンパイラを黙らせる意味で書いている
   const channel = getChannelFromReaction(reaction);
   if (!isTextChannel(channel)) return;
@@ -20,12 +26,38 @@ export const leakMessage = (reaction: MessageReaction) => {
   if (!shouldRunLeak(filters)) return;
 
   try {
-    const emoji = pickEmoji(reaction.client, 'watching_you2');
-    reaction.message.reply(emoji.toString());
+    const twitterService = new TwitterService();
+    const messageContent = reaction.message.content;
+    if (!messageContent) return;
+
+    const tweetResultURL = await twitterService.postTweet(messageContent);
+    const emoji = pickEmoji(reaction.client, 'boomerang');
+    await reaction.message.reply(`${emoji} ${tweetResultURL}`);
   } catch (error: unknown) {
     if (error instanceof EmojiNotFoundError) {
-      reaction.message.reply(
+      await reaction.message.reply(
         `${reaction.emoji} < わりい、使おうとしたやつがないんだわ`
+      );
+      return;
+    }
+
+    if (error instanceof NetworkHandshakeException) {
+      await reaction.message.reply(
+        `${reaction.emoji} < ネットワークの接続で問題が発生したぽいで`
+      );
+      return;
+    }
+
+    if (error instanceof UnauthorizedException) {
+      await reaction.message.reply(
+        `${reaction.emoji} < twitter の認証で死んだんだわ`
+      );
+      return;
+    }
+
+    if (error instanceof ServerErrorException) {
+      await reaction.message.reply(
+        `${reaction.emoji} < Twitter のサービスが死んでるかもしれん`
       );
       return;
     }
