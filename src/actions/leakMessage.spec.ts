192e4d5ea7415b801701b23e47ea4b2784b1890a
huequica <dev@huequica.xyz>
2022-03-06 03:19:34 +0900

    WIP: create action test

--- src/actions/leakMessage.spec.ts
+++ src/actions/leakMessage.spec.ts
@@ -3,6 +3,9 @@ import {
   generateMockMessageReaction,
   GenerateMockMessageReactionOptions,
 } from '@/lib/mocks/messageReaction';
+import { TwitterService } from '@/lib/services/twitter';
+import { TwitterRepository } from '@/lib/repositories/twitter';
+import { mockTwitterTokens } from '@/lib/mocks/env';
 
 describe('🚓 leakMessage', () => {
   it('👮 フィルターを通った場合は reaction.reply.message が発火する', () => {
@@ -26,5 +29,34 @@ describe('🚓 leakMessage', () => {
 
     leakMessage(reactionMock);
     expect(reactionMock.message.reply).not.toHaveBeenCalled();
+  });
+
+  describe('🆗 RESOLVE ALL', () => {
+    it('👮 twitter への投稿まですべて通った場合は twitter のURLを送信する', async () => {
+      const twitterRepository = new TwitterRepository(mockTwitterTokens);
+      const twitterService = new TwitterService(twitterRepository);
+
+      const tweetURL = 'https://twitter.com/mockUser/status/01234567890123';
+      twitterService.postTweet = jest
+        .fn()
+        .mockImplementation(() => Promise.resolve(tweetURL));
+
+      const reactionMock = generateMockMessageReaction();
+
+      reactionMock.client.emojis.cache.find = jest
+        .fn()
+        .mockImplementation(() => ({
+          name: 'boomerang',
+          toString: () => ':boomerang:',
+        }));
+
+      await leakMessage(reactionMock, {
+        twitter: twitterService,
+      });
+
+      expect(reactionMock.message.reply).toHaveBeenCalledWith(
+        `:boomerang: ${tweetURL}`
+      );
+    });
   });
 });
