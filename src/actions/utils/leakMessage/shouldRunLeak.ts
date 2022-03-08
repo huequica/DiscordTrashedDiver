492fc2ae95b4d29ac968222b7aae063a8fc70613
huequica <dev@huequica.xyz>
2022-03-09 00:46:18 +0900

    change filterRules object

--- src/actions/utils/leakMessage/shouldRunLeak.ts
+++ src/actions/utils/leakMessage/shouldRunLeak.ts
@@ -1,13 +1,13 @@
 interface FilterRules {
   channelName: string;
   emojiName: string;
-  messageAuthor: string;
+  isAuthorBot: boolean;
 }
 
 export const shouldRunLeak = ({
   channelName,
   emojiName,
-  messageAuthor,
+  isAuthorBot,
 }: FilterRules): true | void => {
   // "ごみばこ" の名前のチャンネル以外では発火禁止
   if (channelName !== 'ごみばこ') return;
@@ -16,7 +16,7 @@ export const shouldRunLeak = ({
   if (emojiName !== 'troll_face') return;
 
   // メッセージ送信者がbot自身であれば弾く
-  if (messageAuthor === '947934464779120720') return;
+  if (isAuthorBot) return;
 
   return true;
 };
