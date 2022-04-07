fe65bd8a4e5810ec132da55296121161f2f75816
huequica <dev@huequica.xyz>
2022-04-08 03:15:54 +0900

    create remove tweet function filter

---
+++ src/actions/utils/removeTweet/shouldRemoveTweet.ts
@@ -0,0 +1,24 @@
+interface TweetRemoveRuleBases {
+  emojiName: string; // ついた emoji の名前
+  isReactedMessageAuthorBot: boolean; // emoji がついたメッセージが bot の発言かどうか
+  reactorId: string; // リアクションの emoji を追加した人
+  referencedMessageAuthorId: string; //リファレンス元の作者
+}
+
+/**
+ * ツイートを消去する Permission があるかどうかを検査
+ * - reference 元の筆者
+ * - Admins(TODO: 一旦要件から除外)
+ * - botMaintainer(TODO: 一旦要件から除外)
+ * この3者は削除可能とする
+ */
+export const shouldRemoveTweet = (rules: TweetRemoveRuleBases): boolean => {
+  // bot の発言についたものでなければ無視
+  if (!rules.isReactedMessageAuthorBot) return false;
+
+  // emoji.name が `x` でなければ無視
+  if (rules.emojiName !== 'x') return false;
+
+  // 最後に reference 元のユーザーと emoji の追加者が同じ id かどうかを返却
+  return rules.reactorId === rules.referencedMessageAuthorId;
+};
