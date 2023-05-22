1db4c66cb272293ffedc85494827ef76d3bf952e
huequica <dev@huequica.xyz>
2023-05-22 22:09:25 +0900

    fix(repository): 引数の投げ方を修正して 400 を吐かれないようにした

--- src/lib/repositories/twitter.ts
+++ src/lib/repositories/twitter.ts
@@ -30,9 +30,17 @@ export class TwitterRepository {
    * @param mediaIds 画像メディア郡
    */
   async postTweet(content: string, mediaIds?: string[]) {
-    return await this.client.v2.tweet(content, {
-      media: { media_ids: mediaIds },
-    });
+    const requestParams: Parameters<typeof this.client.v2.tweet>[0] = {
+      text: content,
+    };
+
+    if (mediaIds) {
+      requestParams.media = {
+        media_ids: mediaIds,
+      };
+    }
+
+    return await this.client.v2.tweet(requestParams);
   }
 
   /**
